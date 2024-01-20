13d0eb887c792be8a8dcd56e6d906d2ee8098544
LorenzoRonconi00 <lorenzoronconi60@gmail.com>
2024-01-20 11:40:38 +0100

    first commit

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,5 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
-
 export default {
-  light: {
-    text: '#000',
-    background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#fff',
-    background: '#000',
-    tint: tintColorDark,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
-  },
-};
+  primary: '#F6B17A',
+  secondary: '#424769',
+  third: '#2D3250',
+}
